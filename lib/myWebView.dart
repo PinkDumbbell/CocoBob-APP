49ba396862b82684f8a036e8b0605f5a1e0f7e32
은승균 <eunseung-gyun@eunseung-gyun-ui-Macmini.local>
2022-10-30 14:29:55 +0900

    fix: modify userAgent

--- lib/myWebView.dart
+++ lib/myWebView.dart
@@ -23,10 +23,7 @@ class _MyWebViewState extends State<MyWebView> {
           supportZoom: false,
           useShouldOverrideUrlLoading:true,
           mediaPlaybackRequiresUserGesture: false,
-          userAgent: Platform.isIOS ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_2 like Mac OS X) AppleWebKit/605.1.15' +
-              ' (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1' :
-          'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) ' +
-              'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36'
+          userAgent: 'random'
       ),
       android: AndroidInAppWebViewOptions(
         useHybridComposition: true,
